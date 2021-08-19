c27974e3edafedb6dcb81c60652ee678de1b0af0
Satya Ramesh Koppisetti <82876477+rameshkoppisetti@users.noreply.github.com>
2021-08-19 21:14:54 +0530

    Update Loading.js

--- pages/components/Loading.js
+++ pages/components/Loading.js
@@ -1,15 +1,15 @@
 import { Circle } from "better-react-spinkit";
-import image from "next/image";
+//import image from "next/image";
 function Loading() {
   return (
     <center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
       <div>
-        <imageyarn
+    {/*<imageyarn
           src="https://www.freepnglogos.com/uploads/whatsapp-logo-app-png-4.png"
           alt="whatsapp-logo"
           style={{ marginBottom: 10 }}
           height={200}
-        />
+        />*/}
         <Circle color="#3CBC28" size={60} />
       </div>
     </center>
